1a47444568a7037ef93173293d09a73a76be426a
GeorgeSalu <george.salu10@gmail.com>
2022-06-04 10:22:33 -0300

    propriedade de componentes

--- exercicios/pages/basicos/usandoTitulo.jsx
+++ exercicios/pages/basicos/usandoTitulo.jsx
@@ -3,7 +3,14 @@ import Titulo from "../../components/Titulo";
 export default function usandoTitulo() {
     return (
         <div>
-            <Titulo ></Titulo>
+            <Titulo 
+                principal="pagina de cadastro"
+                secundario="incluir, alterar e excluir coisas"/>
+
+            <Titulo 
+                principal="pagina de login"
+                secundario="informe seu eamil e senha"
+                pequeno={true}/>
         </div>
     )
 }
