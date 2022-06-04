fbe14b532824509089712f43638c4526f960a612
GeorgeSalu <george.salu10@gmail.com>
2022-06-04 18:59:18 -0300

    repeticao #02

---
+++ exercicios/data/listaProduto.js
@@ -0,0 +1,8 @@
+import Produto from "../model/produto";
+
+export default [
+    new Produto(1, "ipad", 5434),
+    new Produto(2, "monitor", 5434),
+    new Produto(3, "computador gamer", 5434),
+    new Produto(4, "samsung", 5434),
+]
