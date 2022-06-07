5b60d68fa62c5df8e3c671c3e7dc5c757b97ac68
GeorgeSalu <george.salu10@gmail.com>
2022-06-07 09:12:01 -0300

    desafio contador - resposa #01

--- exercicios/pages/estado/contador.jsx
+++ exercicios/pages/estado/contador.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react"
+
 export default function contador() {
+
+    const [numero, setNumero] = useState(0)
+
+    const inc = () => setNumero(numero + 1)
+    const dec = () => setNumero(numero - 1)
+
     return (
         <div>
             <h1>Contador</h1>
-            <div>Valor: ???</div>
-            <button>-</button>
-            <button>+</button>
+            <div>Valor: {numero}</div>
+            <button onClick={inc}>+</button>
+            <button onClick={dec}>-</button>
         </div>
     )
 }
