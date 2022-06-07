f6503d1a786fb72893176010181b8f47160e4105
GeorgeSalu <george.salu10@gmail.com>
2022-06-07 08:49:02 -0300

    componente com estado

---
+++ exercicios/pages/estado/mouse.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+
+export default function mouse(props) {
+
+    const arrayX = useState(0)
+    let x = arrayX[0]
+    const alterarX = arrayX[1]
+
+    const arrayY = useState(0)
+    let y = arrayY[0]
+    const alterarY = arrayY[1]
+
+    const estilo = {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#222",
+        color: "#fff",
+        height: "100vh"
+    }
+    
+    function quandoMover(ev) {
+        alterarX(ev.clientX) 
+        alterarY(ev.clientY)
+    }
+
+    return (
+        <div style={estilo} onMouseMove={quandoMover}>
+            <span>Eixo x: {x}</span>
+            <span>Eixo y: {y}</span>
+        </div>
+    )
+}
