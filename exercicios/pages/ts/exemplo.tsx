60915ddc41a9840dbab6f810dee0d9b4190aff23
GeorgeSalu <george.salu10@gmail.com>
2022-06-07 13:53:36 -0300

    integrando typescript

---
+++ exercicios/pages/ts/exemplo.tsx
@@ -0,0 +1,9 @@
+import Pessoa from "../../components/Pessoa";
+
+export default function exemploTsx() {
+    return (
+        <div>
+            <Pessoa nome="joao" idade={30} />
+        </div>
+    )
+}
