0ce80cac2eeba5e6cbbf776d3466f21eb09014a3
GeorgeSalu <george.salu10@gmail.com>
2022-06-07 11:49:28 -0300

    componente baseado em classe

---
+++ exercicios/pages/classe/Contador.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+export default class Contador extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Testa classe
+                </h1>
+            </div>
+        )
+    }
+}
