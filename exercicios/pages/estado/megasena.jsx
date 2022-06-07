8f55d24570722a0b3b599b98e720a3efa30a82d5
GeorgeSalu <george.salu10@gmail.com>
2022-06-07 11:40:22 -0300

    desafio mega-sena - resposta 2

---
+++ exercicios/pages/estado/megasena.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { mega } from "../../functions/mega";
+import NumeroDisplay from "../../components/NumeroDisplay"
+
+export default function megasena() {
+    const [qtde, setQtde] = useState(6)
+    const [numeros, setNumeros] = useState(mega(qtde))
+
+    function renderNumeros() {
+        return numeros.map(
+            numero => <NumeroDisplay key={numero} numero={numero} />
+        )
+    }
+
+    return (
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center"
+        }}>
+            <h1>Mega sena</h1>
+            <div style={{  display: "flex" }}>
+                {renderNumeros()}
+            </div>
+            <div>
+                <input type="number" min={6} max={20} value={qtde} 
+                        onChange={ev => setQtde(ev.target.value)} />
+                <button onClick={() => setNumeros(mega(qtde))}>
+                    Gerar apostas
+                </button>
+            </div>
+        </div>
+    )
+}
