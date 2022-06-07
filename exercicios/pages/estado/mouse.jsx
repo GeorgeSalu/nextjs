7169377f7d87d2bd1390306e0ac1a4536fa931ec
GeorgeSalu <george.salu10@gmail.com>
2022-06-07 08:50:05 -0300

    componente com estado

--- exercicios/pages/estado/mouse.jsx
+++ exercicios/pages/estado/mouse.jsx
@@ -2,9 +2,7 @@ import { useState } from "react"
 
 export default function mouse(props) {
 
-    const arrayX = useState(0)
-    let x = arrayX[0]
-    const alterarX = arrayX[1]
+    const [x, alterarX] = useState(0)
 
     const arrayY = useState(0)
     let y = arrayY[0]
