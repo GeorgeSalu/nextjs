14d9c9026d9744f3d07ba16dee3bfe1848b21b1d
GeorgeSalu <george.salu10@gmail.com>
2022-06-10 11:42:18 -0300

    navegando entre paginas #01

--- next-basico/src/pages/rotas/index.jsx
+++ next-basico/src/pages/rotas/index.jsx
@@ -1,7 +1,20 @@
+import Link from 'next/link'
+
 export default function rotas() {
     return (
         <div>
             <h1>Rotas</h1>
+            <ul>
+                <Link href="/rotas/params?id=12&nome=ana">
+                    <li>params</li>
+                </Link>
+                <Link href="/rotas/123/buscar">
+                    <li>buscar</li>
+                </Link>
+                <Link href="/rotas/123/Daniel">
+                    <li>daniel</li>
+                </Link>
+            </ul>
         </div>
     )
 }
--- next-basico/src/pages/rotas/params.jsx
+++ next-basico/src/pages/rotas/params.jsx
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 export default function params() {
     const router = useRouter()
@@ -10,6 +11,9 @@ export default function params() {
     return (
         <div>
             <h1>Rotas Params : {id} e {nome}</h1>
+            <Link href="/rotas">
+                <button>voltar</button>
+            </Link>
         </div>
     )
 }
