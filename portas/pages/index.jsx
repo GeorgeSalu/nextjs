5897d119399f47f0af200144e8cd3fa25a2a673e
GeorgeSalu <george.salu10@gmail.com>
2022-06-11 13:56:10 -0300

    box sizing

--- portas/pages/index.jsx
+++ portas/pages/index.jsx
@@ -2,8 +2,18 @@ import Presente from "../componentes/Presente";
 
 export default function Home() {
   return (
-    <div >
-      <Presente />
+    <div style={{display: "flex"}}>
+      <div style={{
+        width: "200px",
+        height: "200px",
+        backgroundColor: "red",
+        border: "20px solid white"
+      }}></div>
+      <div style={{
+        width: "200px",
+        height: "200px",
+        backgroundColor: "purple"
+      }}></div>
     </div>
   )
 }
