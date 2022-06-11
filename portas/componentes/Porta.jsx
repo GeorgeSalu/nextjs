56cce158724e7c7dd6c53e40597f32a527eae6f4
GeorgeSalu <george.salu10@gmail.com>
2022-06-11 17:23:54 -0300

    corrige nome da classe

--- portas/componentes/Porta.jsx
+++ portas/componentes/Porta.jsx
@@ -2,7 +2,7 @@ import styles from "../styles/Porta.module.css"
 
 export default function Porta(props) {
 
-    const selecionada = props.selecionada ? styles.selecionadda : ''
+    const selecionada = props.selecionada ? styles.selecionada : ''
 
     return (
         <div className={styles.area}>
