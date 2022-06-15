2a34d04b03a467c5f77a65af2aa4bf1ae0781fa0
GeorgeSalu <george.salu10@gmail.com>
2022-06-15 14:57:47 -0300

    lidando com array de portas #01

--- portas/pages/index.tsx
+++ portas/pages/index.tsx
@@ -4,11 +4,13 @@ import PortaModel from "../model/porta";
 
 export default function Home() {
   const [p1, setP1] = useState(new PortaModel(1))
+  const [p2, setP2] = useState(new PortaModel(1))
+  const [p3, setP3] = useState(new PortaModel(1))
   return (
     <div style={{display: "flex"}}>
       <Porta value={p1} onChange={novaPorta => setP1(novaPorta)} />
-      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)} />
-      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)} />
+      <Porta value={p2} onChange={novaPorta => setP2(novaPorta)} />
+      <Porta value={p3} onChange={novaPorta => setP3(novaPorta)} />
     </div>
   )
 }
