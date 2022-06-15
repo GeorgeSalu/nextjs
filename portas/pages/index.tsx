af15dc9e7178a76dccee4a76ee4d47aa6f360cb9
GeorgeSalu <george.salu10@gmail.com>
2022-06-15 14:27:24 -0300

    removendo input

--- portas/pages/index.tsx
+++ portas/pages/index.tsx
@@ -6,7 +6,6 @@ export default function Home() {
   const [p1, setP1] = useState(new PortaModel(1))
   return (
     <div style={{display: "flex", flexDirection: "column"}}>
-      <input type="text" />
       <Porta value={p1} onChange={novaPorta => setP1(novaPorta)} />
     </div>
   )
