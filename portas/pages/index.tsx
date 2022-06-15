d7d91244d2100ab4033f8623b834da1be2e913a6
GeorgeSalu <george.salu10@gmail.com>
2022-06-15 14:10:24 -0300

    adicionando eventos a porta #01

--- portas/pages/index.tsx
+++ portas/pages/index.tsx
@@ -5,8 +5,9 @@ import PortaModel from "../model/porta";
 export default function Home() {
   const [p1, setP1] = useState(new PortaModel(1))
   return (
-    <div style={{display: "flex"}}>
-      <Porta porta={p1} />
+    <div style={{display: "flex", flexDirection: "column"}}>
+      <input type="text" />
+      <Porta value={p1} />
     </div>
   )
 }
