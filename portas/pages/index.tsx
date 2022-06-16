b7a130e30dde62c6918f9e56795e7c9da494978d
GeorgeSalu <george.salu10@gmail.com>
2022-06-16 10:40:53 -0300

    formulario de inicio

--- portas/pages/index.tsx
+++ portas/pages/index.tsx
@@ -1,20 +1,7 @@
-import { useState } from "react";
-import Porta from "../componentes/Porta";
-import { atualizarPortas, criarPortas } from "../functions/porta";
-
 export default function Home() {
-  const [portas, setPortas] = useState(criarPortas(4,3))
-
-  function renderizarPortas() {
-    return portas.map(porta => {
-      return <Porta key={porta.numero} value={porta} 
-                    onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))} />
-    })
-  }
-  
   return (
     <div style={{display: "flex"}}>
-      {renderizarPortas()}
+      <h1>formulario de inicio</h1>
     </div>
   )
 }
