3accec037875390baf01032adbfaf00740a973b3
GeorgeSalu <george.salu10@gmail.com>
2022-06-24 14:20:52 -0300

    componente estatico #04 parte 2

--- renderizacao/pages/estatico4.jsx
+++ renderizacao/pages/estatico4.jsx
@@ -1,4 +1,5 @@
 export async function getStaticProps() {
+    console.log('[server] gerando props para o componente')
     const resp = await fetch("http://localhost:3000/api/produto")
     const produtos = await resp.json()
     return {
@@ -9,6 +10,7 @@ export async function getStaticProps() {
 }
 
 export default function Estatico4(props) {
+    console.log('[cliente] renderizando o componente')
     function renderizarProdutos() {
         return props.produtos.map(produto => {
             return <li key={produto.id}>{produto.nome} tem preco de {produto.preco}</li>
