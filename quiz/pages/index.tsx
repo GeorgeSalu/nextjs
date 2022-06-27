d8bf13886ff4cca455fa278c9cea3f182296c719
GeorgeSalu <george.salu10@gmail.com>
2022-06-27 14:40:23 -0300

    componente resposta #04

--- quiz/pages/index.tsx
+++ quiz/pages/index.tsx
@@ -3,7 +3,7 @@ import QuestaoModel from '../model/questao'
 import RespostaModel from '../model/resposta'
 
 export default function Home() {
-  const questaoTeste = new QuestaoModel(1, 'melhor cor?', [
+  const questaoTeste = new QuestaoModel(1, 'qual é a melhor cor?', [
     RespostaModel.errada('verde'),
     RespostaModel.errada('vermelho'),
     RespostaModel.errada('azul'),
