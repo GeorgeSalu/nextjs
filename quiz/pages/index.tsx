3998d3d8609170b20a7be01e0b5038dd50d32061
GeorgeSalu <george.salu10@gmail.com>
2022-06-27 10:39:20 -0300

    removendo import nao utilizados

--- quiz/pages/index.tsx
+++ quiz/pages/index.tsx
@@ -1,9 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import Questao from '../componentes/Questao'
 import QuestaoModel from '../model/questao'
 import RespostaModel from '../model/resposta'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   const questaoTeste = new QuestaoModel(1, 'melhor cor?', [
