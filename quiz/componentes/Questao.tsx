7e666bbce90340118420ab92db2b53d2728be05b
GeorgeSalu <george.salu10@gmail.com>
2022-06-27 10:58:09 -0300

    componente resposta #01

--- quiz/componentes/Questao.tsx
+++ quiz/componentes/Questao.tsx
@@ -1,6 +1,7 @@
 import styles from '../styles/Questao.module.css'
 import QuestaoModel from "../model/questao";
 import Enunciado from './Enunciado';
+import Resposta from './Resposta';
 
 interface QuestaoProps {
   valor: QuestaoModel
@@ -9,9 +10,23 @@ interface QuestaoProps {
 export default function Questao(props: QuestaoProps) {
   const questao = props.valor
 
+  function rederizarRespostas() {
+    return questao.respostas.map((resposta, i) => {
+      return (
+        <Resposta 
+                valor={resposta}
+                indice={i}
+                letra="A"
+                corLetra="#F2c866"
+               />
+      )
+    })
+  }
+
   return (
     <div className={styles.questao}>
       <Enunciado texto={questao.enunciado} />
+      {rederizarRespostas()}
     </div>
   )
 }
