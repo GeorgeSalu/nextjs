af6a780682aea63d9c5b3f878ac0aaaa6a493277
GeorgeSalu <george.salu10@gmail.com>
2022-06-27 10:24:27 -0300

    iniciando componente questao

---
+++ quiz/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Questao from '../componentes/Questao'
+import QuestaoModel from '../model/questao'
+import RespostaModel from '../model/resposta'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  const questaoTEste = new QuestaoModel(1, 'melhor cor?', [
+    RespostaModel.errada('verde'),
+    RespostaModel.errada('vermelho'),
+    RespostaModel.errada('azul'),
+    RespostaModel.certa('preta'),
+  ])
+  return (
+    <Questao valor={questaoTEste} />
+  )
+}
