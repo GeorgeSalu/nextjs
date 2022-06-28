d4d84a27e1ba932f61d30408cd75fc064c4238ca
GeorgeSalu <george.salu10@gmail.com>
2022-06-28 13:49:22 -0300

    componenten index #01

--- quiz/pages/index.tsx
+++ quiz/pages/index.tsx
@@ -1,4 +1,4 @@
-import {  useState } from 'react'
+import {  useEffect, useState } from 'react'
 import Questionario from '../componentes/Questionario'
 import QuestaoModel from '../model/questao'
 import RespostaModel from '../model/resposta'
@@ -10,8 +10,31 @@ const questaoMock = new QuestaoModel(1, 'Qual é a melhor cor?', [
   RespostaModel.certa('Preta'),
 ])
 
+const BASE_URL = 'http://localhost:3000/api'
+
 export default function Home() {
+  const [idsDasQuestoes, setIdsDasQuestoes] = useState<number[]>([])
   const [questao, setQuestao] = useState(questaoMock)
+
+  async function carrgarIdsDasQuestoes() {
+    const resp = await fetch(`${BASE_URL}/questionario`)
+    const idsDasQuestoes = await resp.json()
+    setIdsDasQuestoes(idsDasQuestoes)
+  }
+
+  async function carrgarQuestoes(id: number) {
+    const resp = await fetch(`${BASE_URL}/questoes/${id}`)
+    const json = await resp.json()
+    console.log(json)
+  }
+
+  useEffect(() => {
+    carrgarIdsDasQuestoes()
+  }, [])
+
+  useEffect(() => {
+    idsDasQuestoes.length > 0 && carrgarQuestoes(idsDasQuestoes[0])
+  }, [idsDasQuestoes])
 
   function respostaFornecida(indice: number) {
     setQuestao(questao.responderCom(indice))
