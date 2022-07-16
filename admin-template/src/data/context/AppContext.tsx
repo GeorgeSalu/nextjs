ba8c36059ed7b74af5f5abb36ba371bf5a50f3f5
GeorgeSalu <george.salu10@gmail.com>
2022-07-16 14:51:50 -0300

    setando tema inicial como dark

--- admin-template/src/data/context/AppContext.tsx
+++ admin-template/src/data/context/AppContext.tsx
@@ -11,7 +11,7 @@ const AppContext = createContext<AppContextProps>({})
 
 export function AppProvider(props) {
 
-  const [tema, setTema] = useState<Tema>('')
+  const [tema, setTema] = useState<Tema>('dark')
 
   function alternarTema(){
     setTema(tema === '' ? 'dark' : '')
