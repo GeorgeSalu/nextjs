6eec19540d18cbf65e6dcce34d264a1b8c582f43
GeorgeSalu <george.salu10@gmail.com>
2022-07-15 21:12:53 -0300

    componente menuLateral #02

---
+++ admin-template/src/pages/ajustes.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/template/Layout";
+
+export default function Ajustes() {
+  return (
+    <Layout titulo="ajustes" subtitulo="ajustes" >
+      <h2>Conteudo</h2>
+    </Layout>
+  )
+}
---
+++ admin-template/src/pages/notificacoes.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/template/Layout";
+
+export default function Notificacoes() {
+  return (
+    <Layout titulo="Notificacoes" subtitulo="notificacao" >
+      <h2>Conteudo</h2>
+    </Layout>
+  )
+}
