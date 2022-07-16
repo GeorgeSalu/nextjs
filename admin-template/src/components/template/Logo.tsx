491769293a6803212311e74837bd6562261508fe
GeorgeSalu <george.salu10@gmail.com>
2022-07-15 23:10:13 -0300

    melhorando componente logo

--- admin-template/src/components/template/Logo.tsx
+++ admin-template/src/components/template/Logo.tsx
@@ -2,7 +2,7 @@ export default function Logo() {
   return (
     <div className={`
       flex flex-col items-center justify-center
-      h-10 w-10 rounded-full
+      h-12 w-12 rounded-full
       bg-white
     `}>
       <div className="h-3 w-3 rounded-full bg-red-600 mb-0.5"></div>
