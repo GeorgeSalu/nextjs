b1cad3edbb56f8d4b67679181cbe326ac7bc2c48
GeorgeSalu <george.salu10@gmail.com>
2022-07-17 13:28:39 -0300

    tela de autenticacao #01

--- admin-template/src/pages/notificacoes.tsx
+++ admin-template/src/pages/notificacoes.tsx
@@ -2,12 +2,8 @@ import Layout from "../components/template/Layout";
 import useAppData from "../data/hook/useAppDate";
 
 export default function Notificacoes() {
-  const dados =  useAppData()
-
   return (
     <Layout titulo="Notificacoes" subtitulo="notificacao" >
-      <h3>{dados.tema}</h3>
-      <button onClick={dados.alternarTema} >Click</button>
     </Layout>
   )
 }
